be258798f1c32d6f1f898fb928d3fe6f9a6ec845
dbotsford <dawsonbotsford@gmail.com>
2014-11-15 06:20:33 -0700

    show total btc

--- js/main.js
+++ js/main.js
@@ -4,11 +4,22 @@ function get_btc(){
         url: "https://api.bitcoinaverage.com/ticker/USD/"
     }).success(function(data){
         $("h2").html("$" + data.last);
-        setTimeout(function(){get_btc();}, 20000);
-    }).responseText;
+    });
 
 }
-function get_wallet_balance(pub_key){
 
-
+function get_wallet_balance(pub_key, callback){
+    var api_key_id = 'a6482aa55ab1ae6fbb8bd12b46ccd80d';
+    var addr = pub_key;
+    var url = 'https://api.chain.com/v2/bitcoin/addresses/' + addr;
+    $.ajax({
+      url: url,
+      data: {'api-key-id': api_key_id},
+      type: 'GET',
+      success: function(data) {
+        console.log(data);
+        $("h1").append((data[0].total.balance)  * .00000001 + " BTC"); 
+        callback(data[0].total.balance);
+      }
+    });
 }
