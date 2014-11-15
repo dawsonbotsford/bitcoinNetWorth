07434e5a47c71747ad3fe8a7a39c8385131cfa41
dbotsford <dawsonbotsford@gmail.com>
2014-11-15 05:48:07 -0700

    Working price update

--- js/main.js
+++ js/main.js
@@ -1,31 +1,14 @@
-function btctousd(btc){
-    var theUrl = "https://api.bitfinex.com/v1/pubticker/btcusd"
-    var responseText = httpGet(theUrl);
-    return responseText;
-/*
-setInterval(function(){
-      // method to be executed;
-    },5000);
-*/
+function get_btc(){
+    var feedback = $.ajax({
+        type: "GET",
+        url: "https://api.bitcoinaverage.com/ticker/USD/"
+    }).success(function(data){
+        $("h2").html("$" + data.last);
+        setTimeout(function(){get_btc();}, 20000);
+    }).responseText;
+
 }
-function httpGet(theUrl)
-{
-    var xmlHttp = null;
-    xmlHttp = new XMLHttpRequest();
-    xmlHttp.open("GET", theUrl, true ); // I tried with true and with false
-    xmlHttp.send();
-    var answer= xmlHttp.responseText;
-    var str = JSON.stringify(answer);
-    console.log(str);
-    var jsonResponse = JSON.parse(str);
-    console.log(jsonResponse);
+function get_wallet_balance(pub_key){
+
+
 }
-/*function httpGet(theUrl){
-  $.getJSON(theUrl, function(data)
-  {
-      $.each(data, function()
-      {
-          console.log(this['title']);
-      })
-  });
-}*/
